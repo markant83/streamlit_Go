4f4f77c1c29883f1ba79d485b7816c9563d5d697
markant83 <markant83@gmail.com>
2026-09-03 21:03:38 +0200

    Update streamlit_Go.py

--- streamlit_Go.py
+++ streamlit_Go.py
@@ -53,7 +53,7 @@ st.title("Mortgage Repayments Calculator - Just for fun 😊")
 
 st.write("### Input Data")
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Home Value", min_value=0, value=500000, step=10000))
+home_value = col1.number_input("Home Value", min_value=0, value=500000, step=10000)
 deposit = col1.number_input("Deposit", min_value=0, value=100000)
 interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=5.5)
 loan_term = col2.number_input("Loan Term (in years)", min_value=1, value=30)
